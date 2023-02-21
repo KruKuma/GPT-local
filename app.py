4e2bd971ee7ee0fa223db0ab4b20896c94872361
Naphatsakorn Khotsombat <naphatsakorn@privatedeal.com>
2023-02-21 13:26:13 +0100

    feat(app): add check for prompt and modify error handling

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def build_api():
 
 
 def generate_text(prompt):
+    check_prompt(prompt)
+
     try:
         completions = openai.Completion.create(
             engine="text-davinci-002",
@@ -26,10 +28,25 @@ def generate_text(prompt):
             temperature=0.7
         )
 
-        message = completions.choices[0].text
-        return message.strip()
+        message = completions.choices[0].text.strip()
+        if not message:
+            return "Could not generate response"
+
+        return message
     except openai.error.APIError as e:
-        return logging.error(e)
+        logging.error(f"OpenAI API returned an API Error: {e}")
+        return "API Error"
+    except openai.error.APIConnectionError as e:
+        logging.error(f"Failed to connect to OpenAI API: {e}")
+        return "Connection Error"
+    except openai.error.RateLimitError as e:
+        logging.error(f"OpenAI API request exceeded rate limit: {e}")
+        return "Rate Limit Exceeded"
+
+
+def check_prompt(prompt):
+    if not isinstance(prompt, str) or len(prompt) == 0:
+        raise ValueError("Invalid prompt")
 
 
 def main():
