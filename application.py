9c46af0121ce30632ec447c7563c961f18a88195
Naphatsakorn Khotsombat <naphatsakorn@privatedeal.com>
2023-04-27 12:56:21 +0200

    refactor(application): reconfigure application for Flask

--- application.py
+++ application.py
@@ -1,43 +1,13 @@
-import os
+import flask
 
-import openai
+import routing
 
-import app
-import utils
+app_config = {
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 
-
-class Application:
-    def __init__(self):
-        project_directory = os.path.dirname(os.path.abspath(__file__))
-        dot_env_file_path = os.path.join(project_directory, ".env")
-        dot_env_file = utils.DotEnvFile(dot_env_file_path)
-        open_api_key = dot_env_file.load_open_ai_key()
-        model_code = dot_env_file.load_model_code()
-
-        self._controller = app.Controller(model_code["chat_model"],
-                                          model_code["text_completion"])
-
-        openai.api_key = open_api_key.get("api_key")
-
-    def define_option(self):
-
-        app_option = 1
-        while app_option != 0:
-            try:
-                option = int(input("\n===== OPTION ====="
-                                   "\n1. Chat"
-                                   "\n2. Text Completion"
-                                   "\n0. Quit"
-                                   "\n>>"))
-
-                if option == 1:
-                    self._controller.chat_request()
-                elif option == 2:
-                    self._controller.text_completion_request()
-                elif option == 0:
-                    app_option = 0
-                else:
-                    print("\nNot an option")
-
-            except ValueError:
-                print("\nNot an integer")
+application = flask.Flask(__name__)
+application.config.from_mapping(app_config)
+router = routing.Router(application)
+router.define_routing()
--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-import application
+import os
 
-application = application.Application()
-application.define_option()
+import openai
+
+import app
+import utils
+
+project_directory = os.path.dirname(os.path.abspath(__file__))
+dot_env_file_path = os.path.join(project_directory, ".env")
+dot_env_file = utils.DotEnvFile(dot_env_file_path)
+open_api_key = dot_env_file.load_open_ai_key()
+model_code = dot_env_file.load_model_code()
+
+_controller = app.Controller(model_code["chat_model"],
+                             model_code["text_completion"])
+
+openai.api_key = open_api_key.get("api_key")
+
+app_option = 1
+while app_option != 0:
+    try:
+        option = int(input("\n===== OPTION ====="
+                           "\n1. Chat"
+                           "\n2. Text Completion"
+                           "\n0. Quit"
+                           "\n>>"))
+
+        if option == 1:
+            _controller.chat_request()
+        elif option == 2:
+            _controller.text_completion_request()
+        elif option == 0:
+            app_option = 0
+        else:
+            print("\nNot an option")
+
+    except ValueError:
+        print("\nNot an integer")
